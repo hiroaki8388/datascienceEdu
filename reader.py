281a0500a9b4ad1c8bdad6d228b1d6296ae56eac
hiroaki <>
2018-07-02 13:16:37 +0900

    Janomeからword分解まで完了

--- reader.py
+++ reader.py
@@ -1,24 +1,34 @@
 from nltk.corpus.reader import PlaintextCorpusReader
 from nltk.tokenize.regexp import RegexpTokenizer
+from janome.tokenizer import Tokenizer as JanomeTokenizer
 import codecs
 
+# root配下に存在するdocsでcorpusの対象とするPATTERN
 DOC_PATTERN = r'(?!\.)[a-z_\s]+/[a-f0-9]+\.txt'
+
 SENT_PATTERN = u'[^　「」！？。]*[！？。]'
 
 
 class JapaneseCorpusReader(PlaintextCorpusReader):
 
-    def __init__(self, root, fields=DOC_PATTERN, encoding='utf8', **kargs):
-        '''
+    def __init__(self, root, fields=DOC_PATTERN, sent_pattern=SENT_PATTERN, encoding='utf8', **kargs):
+        """
         :param root: corpusが入っているdir
         :param fields: 対象となるcorpus
         :param encoding:
-        '''
+        """
 
-        PlaintextCorpusReader.__init__(self, root, fields, sent_tokenizer=
-        RegexpTokenizer(SENT_PATTERN), encoding=encoding)
+        PlaintextCorpusReader.__init__(self, root, fields,
+                                       word_tokenizer=JanomeTokenizer(udic_enc=encoding),
+                                       sent_tokenizer=RegexpTokenizer(sent_pattern),
+                                       encoding=encoding)
 
     def docs(self, fileid=None):
+        """
+
+        :param fileid:
+        :return:
+        """
         for path, enc in self.abspaths(fileid, include_encoding=True):
             with codecs.open(path, 'r', encoding=enc) as f:
                 yield f.read()
