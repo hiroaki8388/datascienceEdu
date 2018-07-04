095a266ebbe5b40b7ca2cfa585a0684e96c91f87
hiroaki <>
2018-07-04 21:28:44 +0900

    one-hotの追加

--- snippets/vectorization.py
+++ snippets/vectorization.py
@@ -1,9 +1,29 @@
+from collections import defaultdict
+
 from sklearn.feature_extraction.text import CountVectorizer
 
 
 def freq_vectorize(copus):
-    """coupusごとに単語の頻度分布をとることでベクトルに変換する
+    """
+    coupusごとに単語の頻度分布をとることでベクトルに変換する
+    :param copus: list(str)
+    :return:
+    """
+    features = defaultdict(int)
+    for token in copus:
+        features[token] = +1
+
+    return features
+
+
+def one_hot_vectorize(corpus):
+    """
+    one-hotによりcorpusをベクトルに変換する
+    :param corpus: list(str)
+    :return:
     """
 
-
-    return vect
+    return {
+        token: True
+        for token in corpus
+    }
