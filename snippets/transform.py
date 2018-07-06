d27f4d6acd46a9c81fb49ff341bd416b2a0d559c
hiroaki <>
2018-07-06 14:05:14 +0900

    word2Vecの追加

--- snippets/transform.py
+++ snippets/transform.py
@@ -1,4 +1,13 @@
+import os
+from functools import reduce
+from operator import add
+import numpy as np
+
+import gensim
 from sklearn.base import BaseEstimator, TransformerMixin
+from gensim.matutils import sparse2full
+
+from snippets.reader import JapaneseCorpusReader
 
 
 class JapaneseTextNormalizer(BaseEstimator, TransformerMixin):
@@ -35,3 +44,76 @@ class JapaneseTextNormalizer(BaseEstimator, TransformerMixin):
         transed = [list(filter(None, [self.normalize(word) for word in sent])) for sent in corpus.sents()]
 
         return transed
+
+
+class GensimVectorizer(BaseEstimator, TransformerMixin):
+    """
+    ScikitLearnにはword2vecのような変換が存在しないので、Gensimより作成する
+    学習済みモデルはこちらのを使う
+    http://aial.shiroyagi.co.jp/2017/02/japanese-word2vec-model-builder/
+    """
+    WORD_DIM = 50
+
+    def __init__(self, path=None):
+        self.path = path
+        self.id2word = None
+
+        self._load()
+
+    def _load(self):
+        if os.path.exists(self.path):
+            # 学習済みモデルのload
+            self.id2word = gensim.models.Word2Vec.load(self.path)
+        else:
+            pass
+
+    def fit(self, sent):
+        """
+
+        :param sent:
+        :return:
+        """
+
+        return self
+
+    def transform(self, sents):
+        """
+        現状Doc2Vecの学習済みモデルが存在しないので、word2vecで変換した単語単位のモデルの平均ベクトルを取る
+        :param sents:
+        :return:
+        """
+        for sent in sents:
+            wordvecs = [self._word2vec(word) for word in sent]
+
+            sentvec = self._mean(wordvecs)
+        
+            yield sentvec
+
+    def _word2vec(self, word):
+        try:
+            word_vec = self.id2word[word]
+            if len(word_vec) == self.WORD_DIM:
+                return word_vec
+            else:
+                return np.zeros(self.WORD_DIM)
+
+        except KeyError:
+            return np.zeros(self.WORD_DIM)
+
+    def _mean(self, wordvecs):
+        try:
+            sentvec = (reduce(add, wordvecs)) / len(wordvecs)
+
+            return sentvec
+
+        except TypeError:
+            pass
+
+
+if __name__ == '__main__':
+    jap = JapaneseCorpusReader(root='./../dataset/corpas', encoding='shift_jis')
+    jap_trans = JapaneseTextNormalizer()
+    traned = jap_trans.transform(corpus=jap)
+    model = GensimVectorizer('./../dataset/latest-ja-word2vec-gensim-model/word2vec.gensim.model')
+    data = model.transform(traned)
+    [vec for vec in data]
--- snippets/vectorization.py
+++ snippets/vectorization.py
@@ -1,29 +1,47 @@
 from collections import defaultdict
 
+from nltk import TextCollection
 from sklearn.feature_extraction.text import CountVectorizer
 
 
-def freq_vectorize(copus):
+def freq_vectorize(sent):
     """
-    coupusごとに単語の頻度分布をとることでベクトルに変換する
-    :param copus: list(str)
+    sentenceごとに単語の頻度分布をとることでベクトルに変換する
+    :param sent: list(str)
     :return:
     """
     features = defaultdict(int)
-    for token in copus:
+    for token in sent:
         features[token] = +1
 
     return features
 
 
-def one_hot_vectorize(corpus):
+def one_hot_vectorize(sent):
     """
-    one-hotによりcorpusをベクトルに変換する
-    :param corpus: list(str)
+    one-hotによりsentenceをベクトルに変換する
+    :param sent: list(str)
     :return:
     """
 
     return {
         token: True
-        for token in corpus
+        for token in sent
     }
+
+
+def tf_idf_vectorize(sents):
+    """
+    tf_idfによりsentssをベクトルに変換する
+    :param corpus: list(list(str))
+    :return:
+    """
+    # 変換するために、全ての単語のlistを生成
+    words = sum(sents, [])
+    word_collection = TextCollection(words)
+
+    for sent in sents:
+        yield {
+            word: word_collection.tf_idf(word, sent)
+            for word in sent
+        }
