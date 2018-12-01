b6e9feb86a512ab089c5b1121a156ba2943442b0
Hiroaki Hasegawa <hiroaki@fringe81.com>
2018-12-01 19:42:38 +0900

    二つの差分が存在する物体を検出

--- on_vscode/Qiita/seek_different.py
+++ on_vscode/Qiita/seek_different.py
@@ -13,24 +13,23 @@ from scipy import fftpack
 
 #%%
 # 間違い探しの画像を読み込む
-file_='on_vscode/Qiita/img/seek_different.jpeg'
-image=io.imread(file_)
-io.imshow(image)
-
-
+file_A='./on_vscode/Qiita/img/book.jpg'
+file_B='./on_vscode/Qiita/img/bottle.jpg'
+image_A=io.imread(file_A)
+image_B=io.imread(file_B)
+# image_A=image_A[image_B.shape]
+io.imshow(image_A)
 
 #%%
-# 画像を分割
-width=image.shape[1]
-image_A=image[:,:width//2]
-image_B=image[:,width//2:]
-
-_,ax=plt.subplots(1,2)
-ax[0].imshow(image_A)
-ax[1].imshow(image_B)
-
+# file_A='./on_vscode/Qiita/img/seek_different.jpeg'
+# image=io.imread(file_A)
+# W=image.shape[1]
+# image_A=image[:,:W//2]
+# image_B=image[:,W//2:]
+# plt.imshow(image_A)
 #%%
 # 変換せずに差分を出力する
+
 io.imshow(image_A-image_B)
 #%%
 # 二次元に変換
@@ -39,7 +38,7 @@ image_B_=image_B.mean(axis=2)
 plt.imshow(image_A_)
 #%%
 # forier変換
-def convertF(image,center=150,f_peak=98):
+def convertF(image,center=500,f_peak=98):
     M,N=image.shape
     F=fftpack.fftn(image)
     F_magnitude=np.abs(F)
@@ -50,54 +49,56 @@ def convertF(image,center=150,f_peak=98):
         M//2-center:M//2+center,
         N//2-center:N//2+center
     ]=0
-    F_magnitude=fftpack.fftshift(F_magnitude)
 
-    peak=np.percentile(F_magnitude,f_peak)
-    F_filtered=np.where(F_magnitude<peak,F,0)
+    
+    peak=np.percentile(F_magnitude,f_peak) 
+    filter_=F_magnitude<peak
+    filter_=fftpack.ifftshift(filter_)
 
-
-    return  F_filtered
+    return  F*filter_.astype(int)
 
 #%% 
 # 差分を抽出
 
-def diff(F_A,F_B,center=150,f_peak=98):
+def diffF(F_A,F_B,center=500,f_peak=98):
     M,N=F_A.shape[:2]
 
-    F_magnitude=abs(F_A-F_B)
+    F_diff=F_A-F_B
 
-    F_magnitude=fftpack.fftshift(F_magnitude)
-    # # ぼんやりとした部分をフィルタの対象から除外する
-    # F_magnitude[
-    #     M//2-center:M//2+center,
-    #     N//2-center:N//2+center
-    # ]=0
-    F_magnitude=fftpack.fftshift(F_magnitude)
+    F_magnitude=np.abs(F_diff)
+    F_magnitude=fftpack.fftshift(F_diff)
 
-    peak=np.percentile(F_magnitude,f_peak)
-    F_filter=np.where(F_magnitude>peak,1,0)
-    
-    
-    return F_filter
+    F_magnitude[
+        M//2-center:M//2+center,
+        N//2-center:N//2+center
+    ]=0
+
+
+    peak=np.percentile(F_magnitude,f_peak) 
+    filter_=F_magnitude>peak
+    filter_=fftpack.ifftshift(filter_)
+
+
+    return F_diff*filter_.astype(int)
 #%%
 # 逆変換
 def convertI(F,center=150,f_peak=98):
 
+    image=fftpack.ifftn(F)
+    image=np.real(image)
 
-    image_filtered=fftpack.ifftn(F_filtered)
-    image_filtered=np.real(image_filtered)
-
-    return image_filtered
+    return image
 
 #%%
-# 差分を可視化
-F_A=convertF(image_A_)
-F_B=convertF(image_B_)
+plt.imshow(convertI(convertF(image_A_,0,f_peak=100),0,0))
+#%%
+# 変換F_A=convertF(image_A_,1000,f_peak=98)
+F_B=convertF(image_B_,1000,f_peak=98)
 
 
-F_diff=diff(F_A,F_B)
-F_A[F_diff]
-# I_diff=convertI(I_diff)
+#%%
+F_diff=diffF(F_A,F_B,1000,f_peak=30)
 
-# plt.imshow(I_diff)
+I_diff=convertI(F_diff)
 
+io.imshow(np.where(I_diff>100,100,0))
